98cfc177fa47bea2023b34810c9d129de94e0296
Zizkito <c.avellanedac@uniandes.edu.co>
2024-10-19 11:02:30 -0500

    Redundancias

--- Caso2/src/Simulacion/Simulacion.java
+++ Caso2/src/Simulacion/Simulacion.java
@@ -6,44 +6,36 @@ public class Simulacion {
     private static int NUM_MARCOS;
     private static final int TIEMPO_ACTUALIZACION = 1;
     private static final int TIEMPO_BIT_R = 2;
-
-    private LinkedHashMap<Integer, Boolean> pageTable;
-    private int[] RAM;
+    private LinkedHashMap<Integer, Boolean> RAM;
     private int hits = 0;
     private int fallos = 0;
-    private List<Integer> referencias;
+    private List<Integer> tablaPaginas;
     private boolean ejecutar = true;
 
-    public Simulacion(int numMarcos, List<Integer> referencias) {
+    public Simulacion(int numMarcos, List<Integer> tablaPaginas) {
         NUM_MARCOS = numMarcos;
-        this.pageTable = new LinkedHashMap<>(NUM_MARCOS, 0.75f, true);
-        this.RAM = new int[NUM_MARCOS];
-        Arrays.fill(RAM, -1);
-        this.referencias = referencias;
+        this.RAM = new LinkedHashMap<>(NUM_MARCOS, 0.75f, true);
+        this.tablaPaginas = tablaPaginas;
     }
 
     public synchronized void cargarPagina(int paginaVirtual) {
-        if (pageTable.containsKey(paginaVirtual)) {
+        if (RAM.containsKey(paginaVirtual)) {
             hits++;
-            pageTable.put(paginaVirtual, true);
+            RAM.put(paginaVirtual, true);
         } else {
             fallos++;
-            if (pageTable.size() >= NUM_MARCOS) {
-                int paginaAReemplazar = pageTable.entrySet().iterator().next().getKey();
-                int marco = getPosicionEnRam(paginaAReemplazar);
-                pageTable.remove(paginaAReemplazar);
-                RAM[marco] = paginaVirtual;
-                pageTable.put(paginaVirtual, true);
+            if (RAM.size() >= NUM_MARCOS) {
+                int paginaAReemplazar = RAM.entrySet().iterator().next().getKey();
+                RAM.remove(paginaAReemplazar);
+                RAM.put(paginaVirtual, true);
             } else {
-                int marcoLibre = pageTable.size();
-                RAM[marcoLibre] = paginaVirtual;
-                pageTable.put(paginaVirtual, true);
+                RAM.put(paginaVirtual, true);
             }
         }
     }
 
     public void iniciar() {
-        PaginaThread paginaThread = this.new PaginaThread(referencias);
+        PaginaThread paginaThread = this.new PaginaThread(tablaPaginas);
         BitRThread bitRThread = this.new BitRThread();
         paginaThread.start();
         bitRThread.start();
@@ -56,38 +48,26 @@ public class Simulacion {
         this.mostrarEstado();
     }
 
-    private synchronized int getPosicionEnRam(int paginaVirtual) {
-        for (int i = 0; i < RAM.length; i++) {
-            if (RAM[i] == paginaVirtual) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public synchronized void mostrarEstado() {
-        System.out.println("RAM: " + Arrays.toString(RAM));
-        System.out.println("Tabla de páginas: " + pageTable);
+    public void mostrarEstado() {
+        System.out.println("RAM: " + RAM);
         System.out.println("Hits: " + hits + ", Fallos: " + fallos);
-        System.out.println("Porcentaje de Hits sobre Misses: " + (((float) hits / (float) referencias.size()) * 100)+"%");
+        System.out.println("Porcentaje de Hits sobre Misses: " + (((float) hits / (float) tablaPaginas.size()) * 100)+"%");
     }
 
     public class PaginaThread extends Thread {
-        private List<Integer> referencias;
+        private List<Integer> tablaPaginas;
 
-        public PaginaThread(List<Integer> referencias) {
-            this.referencias = referencias;
+        public PaginaThread(List<Integer> tablaPaginas) {
+            this.tablaPaginas = tablaPaginas;
         }
 
         @Override
         public void run() {
-            double total = (double) referencias.size();
+            double total = (double) tablaPaginas.size();
             double progreso = 0.0;
             int porcentajeMostrado = 0;
-            for (int referencia : referencias) {
-                synchronized (Simulacion.this) {
+            for (int referencia : tablaPaginas) {
                     cargarPagina(referencia);
-                }
                 progreso++;
                 int porcentaje = (int) Math.round(((progreso) / total) * 100);
                 if (porcentaje % 5 == 0 && porcentaje != porcentajeMostrado) {
@@ -110,7 +90,7 @@ public class Simulacion {
         public void run() {
             while (ejecutar) {
                 synchronized (Simulacion.this) {
-                    for (Map.Entry<Integer, Boolean> entry : pageTable.entrySet()) {
+                    for (Map.Entry<Integer, Boolean> entry : RAM.entrySet()) {
                         entry.setValue(false);
                     }
                 }
